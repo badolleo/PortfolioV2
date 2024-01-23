d690789029eabc6cb4a93b33f86b0a4a3d43099c
Badol Leo <leo.badol@etu.u-bordeaux.fr>
2024-01-23 21:20:34 +0100

    ajout du projet

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ['./*.html'],
+    theme : {
+        extend: {
+            colors: {
+                'orange-1' : '#E3AC1C',
+                'background-white' : 'rgb(243 244 246)',
+                'transparent-1' : 'rgba(0,0,0,0.5)',
+            },
+        },
+    },
+    plugins: [] ,
+}
